88ae7f3fc415e2a872498a4346fa0b6487792409
iisxrxh <74046058+iisxrxh@users.noreply.github.com>
2021-05-29 21:53:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+var backgroundI;
+var snowI;
+
+
+
+function setup() {
+  createCanvas(800,400);
+ 
+  
+}
+
+function preload(){
+  backgroundI = loadImage("snow1.jpg")
+  snowI = loadImage("snow5.webp")
+
+
+}
+
+function draw() {
+  background(backgroundI);  
+
+ if(frameCount%100===0){
+    var snow = createSprite(random(30, 780), -20, 10, 10)
+    snow.velocityY = 5;
+    snow.addImage(snowI)
+    snow.scale = 0.1;
+  }
+  drawSprites();
+}
